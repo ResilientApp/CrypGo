dc446b570d8a871c3d07a308a03f5c0cd65d6162
rohanmalige <rmmalige@ucdavis.edu>
2023-11-06 11:26:27 -0800

    added testfile

--- Frontend/components/Test.js
+++ Frontend/components/Test.js
@@ -1,28 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 
 const TestPage = () => {
+  const [firstInput, setFirstInput] = useState('');
+  const [secondInput, setSecondInput] = useState('');
+  const [thirdInput, setThirdInput] = useState('');
+  const [foruthInput, setFourthInput] = useState('');
+  const [combinedInput, setCombinedInput] = useState('');
+
+  const handlePressA = () => {
+    // Combine the first two input values and set it to the last text input
+    setCombinedInput(firstInput + ' ' + secondInput);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Test Page</Text>
-      <View style={styles.inputContainer}>
-        <View style={styles.input} />
-        <View style={styles.input} />
-        <View style={styles.input} />
-        <View style={styles.input} />
-      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Type here..."
+        onChangeText={setFirstInput}
+        value={firstInput}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Type here..."
+        onChangeText={setSecondInput}
+        value={secondInput}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Type here..."
+        onChangeText={setThirdInput}
+        value={thirdInput}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Type here..."
+        onChangeText={setFourthInput}
+        value={foruthInput}
+      />
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonA]}>
+        <TouchableOpacity style={[styles.button, styles.buttonA]} onPress={handlePressA}>
           <Text>A</Text>
         </TouchableOpacity>
         <TouchableOpacity style={[styles.button, styles.buttonB]}>
           <Text>B</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.inputContainer}>
-        <View style={styles.input} />
-      </View>
-      <View style={styles.input} />
+      <TextInput
+        style={styles.input}
+        placeholder="Results show here..."
+        value={combinedInput}
+        editable={false} // Make this TextInput read-only
+      />
     </View>
   );
 };
@@ -40,15 +71,13 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
   },
-  inputContainer: {
-    alignSelf: 'stretch',
-    marginBottom: 20,
-  },
   input: {
     backgroundColor: 'white',
     height: 50,
     borderRadius: 10,
     marginBottom: 10,
+    alignSelf: 'stretch',
+    paddingHorizontal: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
@@ -58,6 +87,8 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     marginHorizontal: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonA: {
     backgroundColor: 'darkgrey',
