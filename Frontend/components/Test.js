991afaee67df8d2d2361040868f35290bdef3070
rohanmalige <rmmalige@ucdavis.edu>
2023-11-06 11:08:10 -0800

    added testfile

--- Frontend/components/Test.js
+++ Frontend/components/Test.js
@@ -1,13 +1,70 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-function Test() {
+const TestPage = () => {
   return (
-    <View>
-      <Text>Test Screen</Text>
-      {/* Your content goes here */}
+    <View style={styles.container}>
+      <Text style={styles.header}>Test Page</Text>
+      <View style={styles.inputContainer}>
+        <View style={styles.input} />
+        <View style={styles.input} />
+        <View style={styles.input} />
+        <View style={styles.input} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.button, styles.buttonA]}>
+          <Text>A</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.buttonB]}>
+          <Text>B</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.inputContainer}>
+        <View style={styles.input} />
+      </View>
+      <View style={styles.input} />
     </View>
   );
-}
+};
 
-export default Test;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  header: {
+    color: 'green',
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  inputContainer: {
+    alignSelf: 'stretch',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: 'white',
+    height: 50,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  button: {
+    padding: 15,
+    borderRadius: 10,
+    marginHorizontal: 10,
+  },
+  buttonA: {
+    backgroundColor: 'darkgrey',
+  },
+  buttonB: {
+    backgroundColor: 'grey',
+  },
+});
+
+export default TestPage;
